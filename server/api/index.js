c79ee75fef94de0c517f278db96ffaea8d9cdacb
Bharath <beastobharath12@gmail.com>
2026-08-18 19:06:34 +0530

    Add detailed error capturing in api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,12 @@
-const app = require('../server');
-
-module.exports = (req, res) => {
-  return app(req, res);
+module.exports = async (req, res) => {
+  try {
+    const app = require('../server');
+    return app(req, res);
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Serverless Handler Initialization Error',
+      message: err.message,
+      stack: err.stack,
+    });
+  }
 };
